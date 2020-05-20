3fbc3a0f86c3c2f3c97cae1ecc055ea73425a245
Christopher Foley <chrisninetyone@gmail.com>
2020-05-19 23:52:42 -0400

    implemented sidebar

--- src/index.js
+++ src/index.js
@@ -24,11 +24,22 @@ class Application extends React.Component {
 			center: [this.state.lng, this.state.lat],
 			zoom: this.state.zoom
 		});
+
+		map.on('move', () => {
+			this.setState({
+				lng: map.getCenter().lng.toFixed(4),
+				lat: map.getCenter().lat.toFixed(4),
+				zoom: map.getZoom().toFixed(2)
+			});
+		});
 	}
 
 	render() {
 		return (
 			<div>
+				<div className='sidebarStyle' >
+					<div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
+				</div>
 				<div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
 			</div>
 		);
