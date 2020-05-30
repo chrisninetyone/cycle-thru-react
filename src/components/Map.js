c0a7e37ae43a28ac0ca5827c2c90159bbed27703
Christopher Foley <chrisninetyone@gmail.com>
2020-05-29 23:40:17 -0400

    Better setup of Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,90 @@
-// import React from 'react';
+import React from 'react';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = `pk.eyJ1Ijoic3dvbGZzb24iLCJhIjoiY2p2cTNmNjJqMGplaDQ5bW85eGI0a3pvcyJ9.UmldkLqn5ujgok-SwUf2Lg`;
+
+class Map extends React.Component {
+	constructor(props) {
+        super(props);
+        console.log('PROPS', props)
+		this.state = {
+            lng: 0,
+            lat: 0,
+			zoom: 14,
+		};
+	}
+
+	componentDidMount() {
+        // const currentLoc = {}
+        // if (navigator.geolocation) {
+        //     navigator.geolocation.getCurrentPosition(
+        //         ({ coords: { latitude, longitude } }) => {
+        //             console.log('Before SET STATE', this.state)
+        //             this.setState({
+        //                 lng: longitude,
+        //                 lat: latitude,
+        //             })
+        //             console.log('AFTER SET STATE', this.state)
+        //             currentLoc.lng = this.state.lng
+        //             currentLoc.lat = this.state.lat;
+        //             console.log('load from current location', currentLoc);
+        //         }
+        //     );
+        // } else {
+        //     console.log('load from hardcoded location');
+        //     this.setState({
+        //         lng: 115.1304015,
+        //         lat: -8.6539913,
+        //     })
+        //     console.log('ELSE BLOCK', this.state)
+        // }
+        // this.setState({
+        //     lng: currentLoc.lng,
+        //     lat: currentLoc.lat,
+        // })
+        // console.log(this.state, 'BEFORE MAP')
+
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [this.state.lng, this.state.lat],
+            zoom: this.state.zoom
+        });
+
+        map.on('move', () => {
+            this.setState({
+                lng: map.getCenter().lng.toFixed(4),
+                lat: map.getCenter().lat.toFixed(4),
+                zoom: map.getZoom().toFixed(2)
+            });
+        });
+
+        const geolocate = new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        });
+
+        map.addControl(geolocate);
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="sidebarStyle">
+					<div>
+						Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{' '}
+						{this.state.zoom}
+					</div>
+				</div>
+				<div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+			</div>
+		);
+	}
+}
+
+export default Map;
 
 // const Map = () => {
 //     const initMapbox = (currentLocation) => {
--- src/index.js
+++ src/index.js
@@ -1,52 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import mapboxgl from 'mapbox-gl';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-mapboxgl.accessToken = `pk.eyJ1Ijoic3dvbGZzb24iLCJhIjoiY2p2cTNmNjJqMGplaDQ5bW85eGI0a3pvcyJ9.UmldkLqn5ujgok-SwUf2Lg`;
-
-class Application extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			lng: 5,
-			lat: 34,
-			zoom: 2
-		};
-	}
-
-	componentDidMount() {
-		const map = new mapboxgl.Map({
-			container: this.mapContainer,
-			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [this.state.lng, this.state.lat],
-			zoom: this.state.zoom
-		});
-
-		map.on('move', () => {
-			this.setState({
-				lng: map.getCenter().lng.toFixed(4),
-				lat: map.getCenter().lat.toFixed(4),
-				zoom: map.getZoom().toFixed(2)
-			});
-		});
-	}
-
-	render() {
-		return (
-			<div>
-				<div className='sidebarStyle' >
-					<div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
-				</div>
-				<div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
-			</div>
-		);
-	}
-}
-
-ReactDOM.render(<Application />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
